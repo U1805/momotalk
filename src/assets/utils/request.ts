4c87c89daaaa1126093b7f3e4974325c8da3a346
带dai <dai.0v0@qq.com>
2023-12-05 02:00:17 +0800

    feat: support Traditional Chinese nicknames

--- src/assets/utils/request.ts
+++ src/assets/utils/request.ts
@@ -1,17 +1,18 @@
 import axios from 'axios'
 import { myStudent } from './interface'
+import { Traditionalized } from './tw_cn'
 
 const prefixTable: { [key: string]: string[] } = {
     bunnygirl: ['兔', '兔女郎'],
     casual: ['私服', '滑板'],
-    cheerleader: ['应援', '啦啦队', '拉拉队'],
+    cheerleader: ['应援', '应援服', '啦啦队', '拉拉队'],
     christmas: ['圣诞'],
-    gym: ['体', '体操', '运动服', '运动'],
+    gym: ['体', '体操', '体操服', '运动', '运动服'],
     hotspring: ['温泉'],
     maid: ['女仆'],
-    newyear: ['春', '新', '新春', '新年', '正月'],
+    newyear: ['新', '春', '新春', '新年', '正月'],
     riding: ['单车', '骑行'],
-    swimsuit: ['泳装', '水'],
+    swimsuit: ['水', '泳装'],
     young: ['幼', '幼女']
 }
 
@@ -34,33 +35,36 @@ const getSchale = async (lng: string) => {
     const schale = await getData(`https://schale.gg/data/${lng}/students.min.json`)
     const results: myStudent[] = []
     for (const schaleItem of schale) {
+        const localItem = local.find((ele) => ele.Id === schaleItem.Id)
+
         const newStudent: myStudent = {
             Id: schaleItem.Id,
             Name: schaleItem.Name,
             Birthday: schaleItem.Birthday,
-            Avatar: [getSchaleImg(schaleItem.Id)],
-            Bio: '',
-            Nickname: [schaleItem.PathName],
-            School: schaleItem.School.replace('Sakugawa', 'ETC'),
+            Avatar: [getSchaleImg(schaleItem.Id), ...(localItem ? localItem.Avatar : [])],
+            Bio: (localItem && localItem.Bio[lng]) ? localItem.Bio[lng] : "",
+            Nickname: [schaleItem.PathName, ...(localItem ? localItem.Nickname : [])],
+            School: (localItem && localItem.School) ? localItem.School : schaleItem.School,
             cnt: 0
         }
-        const localItem = local.find((ele) => ele.Id === newStudent.Id)
-        if (localItem) {
-            newStudent.Avatar = newStudent.Avatar.concat(localItem.Avatar)
-            newStudent.Bio = localItem.Bio[lng]
-            newStudent.Nickname = newStudent.Nickname.concat(localItem.Nickname)
-            // add prefix
-            if (localItem.related) {
-                const relatedInfo: [number, string] = localItem.related
-                const relatedItem = results.find((ele) => ele.Id === relatedInfo[0])
-                const prefixs = prefixTable[relatedInfo[1]]
-                for (const prefix of prefixs) {
-                    newStudent.Nickname.push(prefix + relatedItem!.Name)
-                    for (const nickname of relatedItem!.Nickname)
-                        newStudent.Nickname.push(prefix + nickname)
-                }
+
+        // filling empty bio
+        if (localItem && lng == 'en' && !localItem.Bio['en'])
+            newStudent.Bio = localItem.Bio['jp']
+        if (localItem && lng == 'tw' && !localItem.Bio['tw'])
+            newStudent.Bio = Traditionalized(localItem.Bio['zh']) as string
+
+        // generating nicknames: add prefix
+        if (localItem && localItem.related) {
+            const relatedInfo: [number, string] = localItem.related
+            const relatedItem = results.find((ele) => ele.Id === relatedInfo[0])
+            const prefixs = prefixTable[relatedInfo[1]]
+            for (const prefix of prefixs) {
+                newStudent.Nickname.push(prefix + relatedItem!.Name)
+                newStudent.Nickname.push(...relatedItem!.Nickname.map(nickname => prefix + nickname))
             }
         }
+        newStudent.Nickname.push(...Traditionalized(newStudent.Nickname))
         results.push(newStudent)
     }
     return results
@@ -74,13 +78,12 @@ const getLocal = async (lng: string) => {
             Id: localItem.Id,
             Name: localItem.Name[lng] ? localItem.Name[lng] : localItem.Name['en'],
             Birthday: '???',
-            Avatar: ['/momotalk/Avatars/' + localItem.Nickname[0] + '.webp'],
+            Avatar: [`/momotalk/Avatars/${localItem.Nickname[0]}.webp`, ...localItem.Avatar],
             Bio: localItem.Bio[lng] ? localItem.Bio[lng] : localItem.Bio['en'],
-            Nickname: localItem.Nickname,
+            Nickname: [...localItem.Nickname, ...Traditionalized(localItem.Nickname)],
             School: localItem.School ? localItem.School : '',
             cnt: 0
         }
-        newStudent.Avatar = newStudent.Avatar.concat(localItem.Avatar)
         results.push(newStudent)
     }
     return results
@@ -98,7 +101,7 @@ const getMessage = async (storyid: string, story: string) => {
 }
 
 const getStickers = async (student: number) => {
-    const res = await getData(`/momotalk/Stories/${student}/Stickers.json`) 
+    const res = await getData(`/momotalk/Stories/${student}/Stickers.json`)
     return res
 }
 
