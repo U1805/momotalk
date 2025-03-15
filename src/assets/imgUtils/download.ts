b4d17c719b29be89a0d097eb896a0e73b9c8e8ed
bobby233 <mczsjzsjz@icloud.com>
2025-03-15 22:41:46 +0800

    fix: checking browser zoom in Firefox

--- src/assets/imgUtils/download.ts
+++ src/assets/imgUtils/download.ts
@@ -1,14 +1,6 @@
 import { domtoimage } from './dom-to-image'
-import i18n from '@/locales/i18n'
 
 const download = () => {
-    // 检查浏览器缩放，缩放可能导致文字溢出 Check browser zoom, which may cause the last overflow
-    // TODO: this way does not work on firefox. looking forward to a better detection method.
-    const radio = window.outerWidth / window.innerWidth  
-    if (Math.abs(radio - 1) > 0.05){
-        const ans = confirm(i18n.global.t('warnZoom').replace("%radio%", (radio*100).toFixed(0)+"%"))
-        if (!ans) return
-    }
     const node = document.getElementsByClassName('talk-list')[0]
     const indicators = document.getElementsByClassName('insert-indicator')
     const indicator = indicators.length>0 ? indicators[0] : document.createElement("div")
